3954281ca92f010e20a72d24a41003afcb4d3188
leex <leex@hnu.edu.cn>
2022-01-05 00:19:48 +0800

    init 2022 study dirctory layout

---
+++ 2022/leetcode.cpp
@@ -0,0 +1,22 @@
+//
+// Created by lix on 1/5/22.
+//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <gtest/gtest.h>
+#include <unordered_map>
+#include <map>
+#include <string>
+#include <climits>
+#include <queue>
+#include <list>
+#include <unordered_set>
+
+using namespace std;
+
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
---
+++ common/common_types.h
@@ -0,0 +1,8 @@
+//
+// Created by lix on 1/4/22.
+//
+
+#ifndef LEET_CODE_COMMON_TYPES_H
+#define LEET_CODE_COMMON_TYPES_H
+
+#endif //LEET_CODE_COMMON_TYPES_H
