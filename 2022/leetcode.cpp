0a61459a252c6386ca11913c6c55c1884d0eaee3
leex <leex@hnu.edu.cn>
2022-01-10 00:17:32 +0800

    init leetcode upload

--- 2022/leetcode.cpp
+++ 2022/leetcode.cpp
@@ -15,6 +15,40 @@
 
 using namespace std;
 
+namespace greedy_algorithm {
+
+class Solution {
+ public:
+   int findContentChildren(vector<int>& g,vector<int>& s) {
+    std::sort(g.begin(), g.end());
+    std::sort(s.begin(), s.end());
+    int num = 0;
+    size_t g_idx = 0;
+    size_t s_idx = 0;
+    while(g_idx < g.size() && s_idx < s.size()){
+      if(g[g_idx] <= s[s_idx]){
+        num++;
+        g_idx++;
+      }
+      s_idx++;
+    }
+    return num;
+  }
+};
+
+TEST(easy, T455) {
+  Solution slo;
+  std::vector<int> g,s;
+  g={1,2,3};
+  s={1,1};
+  EXPECT_EQ(1, slo.findContentChildren(g,s));
+  g={1,2};
+  s={1,2,3};
+  EXPECT_EQ(2, slo.findContentChildren(g,s));
+}
+
+}
+
 
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
