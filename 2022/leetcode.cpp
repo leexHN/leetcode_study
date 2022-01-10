8ebfe580ca2d7cd82cf2131561bcde5c61d1edea
leex <leex@hnu.edu.cn>
2022-01-11 00:25:23 +0800

    T135做题时间过长，没解出，需要重做

--- 2022/leetcode.cpp
+++ 2022/leetcode.cpp
@@ -12,31 +12,32 @@
 #include <queue>
 #include <list>
 #include <unordered_set>
+#include <numeric>
 
 using namespace std;
 
 namespace greedy_algorithm {
 
-class Solution {
- public:
-   int findContentChildren(vector<int>& g,vector<int>& s) {
-    std::sort(g.begin(), g.end());
-    std::sort(s.begin(), s.end());
-    int num = 0;
-    size_t g_idx = 0;
-    size_t s_idx = 0;
-    while(g_idx < g.size() && s_idx < s.size()){
-      if(g[g_idx] <= s[s_idx]){
-        num++;
-        g_idx++;
-      }
-      s_idx++;
-    }
-    return num;
-  }
-};
-
 TEST(easy, T455) {
+  class Solution {
+   public:
+    int findContentChildren(vector<int>& g,vector<int>& s) {
+      std::sort(g.begin(), g.end());
+      std::sort(s.begin(), s.end());
+      int num = 0;
+      size_t g_idx = 0;
+      size_t s_idx = 0;
+      while(g_idx < g.size() && s_idx < s.size()){
+        if(g[g_idx] <= s[s_idx]){
+          num++;
+          g_idx++;
+        }
+        s_idx++;
+      }
+      return num;
+    }
+  };
+
   Solution slo;
   std::vector<int> g,s;
   g={1,2,3};
@@ -46,6 +47,69 @@ TEST(easy, T455) {
   s={1,2,3};
   EXPECT_EQ(2, slo.findContentChildren(g,s));
 }
+
+TEST(hard, T135) { //  需要重做
+  /* 错误解法!!!!
+  class Solution {
+   public:
+    // 对每个孩子而言，自己就是局部最优，先初始化每个孩子的糖果为1,每个孩子左边最优+右边最优==全局最优
+    // 即先让每个孩子左边最优，基于这个符合左边最优的优化右边最优
+    int candy(vector<int>& ratings) {
+      if (ratings.empty()) {
+        return 0;
+      }
+      std::vector<int> candy_vec(ratings.size(), 1);
+      int num_candy = ratings.size();
+      // optimize left
+      for (int i = 1; i < ratings.size(); ++i) {
+        if (ratings[i] > ratings[i-1] && candy_vec[i] <= candy_vec[i-1]) { // local mismatch 自己糖果不匹配
+          candy_vec[i] = candy_vec[i-1]+1;
+        }
+      }
+      // optimize right  右边最优并且不破坏左边最优
+      for (int i = 0; i < ratings.size() - 1; i++) {
+        if (ratings[i] > ratings[i + 1] && candy_vec[i] <= candy_vec[i + 1] && i == 1) {
+          candy_vec[i] = candy_vec[i + 1] + 1;
+        } else { // my have left, must keep left optimization
+          if (ratings[i] > ratings[i - 1] && candy_vec[i] <= candy_vec[i - 1]) { // keep left optimization
+            candy_vec[i] = candy_vec[i - 1] + 1;
+          }
+          if (ratings[i] > ratings[i + 1] && candy_vec[i] <= candy_vec[i + 1]) { // let right optimization
+            candy_vec[i] = candy_vec[i + 1] + 1; // this process may cause left not optimize
+          }
+        }
+      }
+
+      return std::accumulate(candy_vec.begin(), candy_vec.end(), 0);
+    }
+  };
+   */
+
+  class Solution {
+   public:
+    int candy(vector<int>& ratings) {
+      std::vector<int> candy_vec(ratings.size(), 1);
+      for (size_t i=1; i< candy_vec.size(); i++) {
+        if (ratings[i] > ratings[i-1])  //其实这里还有一个条件，candy_vec[i] <= candy_vec[i-1],自动满足了
+          candy_vec[i] = candy_vec[i-1]+1;
+      }
+      //左边都最优了，如何利用这一个条件，左边最优最后一个自动满足条件（左右最优），从倒数第二个开始，不改变左边最优，改变自己使得右边最优
+      for (int i=candy_vec.size()-2; i>=0; i--) {
+        if (ratings[i] > ratings[i+1] && candy_vec[i] <= candy_vec[i+1]) {
+          candy_vec[i] = candy_vec[i+1] +1;
+        }
+      }
+      return std::accumulate(candy_vec.begin(), candy_vec.end(), 0);
+    }
+  };
+  Solution slo;
+  std::vector<int> ratings;
+  ratings={1,0,2};
+  EXPECT_EQ(5,slo.candy(ratings));
+  ratings={1,2,87,87,87,2,1};
+  EXPECT_EQ(13,slo.candy(ratings));
+}
+
 
 }
 
