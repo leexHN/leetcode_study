1e4c73d7cd67eea300caccbd7cc37bf976df5615
leex <leex@hnu.edu.cn>
2022-01-13 00:17:36 +0800

    终于第一次提交就通过了mid难度的题目
    Signed-off-by: leex <leex@hnu.edu.cn>

--- 2022/leetcode.cpp
+++ 2022/leetcode.cpp
@@ -21,14 +21,14 @@ namespace greedy_algorithm {
 TEST(easy, T455) {
   class Solution {
    public:
-    int findContentChildren(vector<int>& g,vector<int>& s) {
+    int findContentChildren(vector<int> &g, vector<int> &s) {
       std::sort(g.begin(), g.end());
       std::sort(s.begin(), s.end());
       int num = 0;
       size_t g_idx = 0;
       size_t s_idx = 0;
-      while(g_idx < g.size() && s_idx < s.size()){
-        if(g[g_idx] <= s[s_idx]){
+      while (g_idx < g.size() && s_idx < s.size()) {
+        if (g[g_idx] <= s[s_idx]) {
           num++;
           g_idx++;
         }
@@ -39,13 +39,13 @@ TEST(easy, T455) {
   };
 
   Solution slo;
-  std::vector<int> g,s;
-  g={1,2,3};
-  s={1,1};
-  EXPECT_EQ(1, slo.findContentChildren(g,s));
-  g={1,2};
-  s={1,2,3};
-  EXPECT_EQ(2, slo.findContentChildren(g,s));
+  std::vector<int> g, s;
+  g = {1, 2, 3};
+  s = {1, 1};
+  EXPECT_EQ(1, slo.findContentChildren(g, s));
+  g = {1, 2};
+  s = {1, 2, 3};
+  EXPECT_EQ(2, slo.findContentChildren(g, s));
 }
 
 TEST(hard, T135) { //  需要重做
@@ -87,16 +87,16 @@ TEST(hard, T135) { //  需要重做
 
   class Solution {
    public:
-    int candy(vector<int>& ratings) {
+    int candy(vector<int> &ratings) {
       std::vector<int> candy_vec(ratings.size(), 1);
-      for (size_t i=1; i< candy_vec.size(); i++) {
-        if (ratings[i] > ratings[i-1])  //其实这里还有一个条件，candy_vec[i] <= candy_vec[i-1],自动满足了
-          candy_vec[i] = candy_vec[i-1]+1;
+      for (size_t i = 1; i < candy_vec.size(); i++) {
+        if (ratings[i] > ratings[i - 1])  //其实这里还有一个条件，candy_vec[i] <= candy_vec[i-1],自动满足了
+          candy_vec[i] = candy_vec[i - 1] + 1;
       }
       //左边都最优了，如何利用这一个条件，左边最优最后一个自动满足条件（左右最优），从倒数第二个开始，不改变左边最优，改变自己使得右边最优
-      for (int i=candy_vec.size()-2; i>=0; i--) {
-        if (ratings[i] > ratings[i+1] && candy_vec[i] <= candy_vec[i+1]) {
-          candy_vec[i] = candy_vec[i+1] +1;
+      for (int i = candy_vec.size() - 2; i >= 0; i--) {
+        if (ratings[i] > ratings[i + 1] && candy_vec[i] <= candy_vec[i + 1]) {
+          candy_vec[i] = candy_vec[i + 1] + 1;
         }
       }
       return std::accumulate(candy_vec.begin(), candy_vec.end(), 0);
@@ -104,14 +104,215 @@ TEST(hard, T135) { //  需要重做
   };
   Solution slo;
   std::vector<int> ratings;
-  ratings={1,0,2};
-  EXPECT_EQ(5,slo.candy(ratings));
-  ratings={1,2,87,87,87,2,1};
-  EXPECT_EQ(13,slo.candy(ratings));
+  ratings = {1, 0, 2};
+  EXPECT_EQ(5, slo.candy(ratings));
+  ratings = {1, 2, 87, 87, 87, 2, 1};
+  EXPECT_EQ(13, slo.candy(ratings));
+}
+
+TEST(easy, T605) {
+  class Solution {
+   public:
+//    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+//      int flower_inserted = 0;
+//      int idx = 0;
+//      if (n==0) {
+//        return true;
+//      }
+//      if (flowerbed.size() <= 2) {
+//        for(int i=0; i<flowerbed.size();i++) {
+//          if (flowerbed[i]==1) {
+//            return false;
+//          }
+//        }
+//        if (n==1)
+//          return true;
+//        else
+//          return false;
+//      }
+//      while(idx < flowerbed.size()) {
+//        if(flowerbed[idx] == 1) {
+//          idx+=2;
+//          continue;
+//        }
+//        if (idx+1 < flowerbed.size() && idx-1 >=0){
+//          if(flowerbed[idx-1] ==0 && flowerbed[idx+1] == 0) {
+//            flowerbed[idx] = 1;
+//            flower_inserted++;
+//            idx+=2;
+//          } else {
+//            idx++;
+//          }
+//        } else if(idx == 0) {
+//          if (flowerbed[idx+1] == 1){
+//            idx+=2;
+//          }else{
+//            flower_inserted++;
+//            flowerbed[idx] = 1;
+//            idx+=2;
+//          }
+//        } else {
+//          if (flowerbed[idx-1] == 1) {
+//            idx+=2;
+//          } else {
+//            flower_inserted++;
+//            flowerbed[idx] = 1;
+//            idx+=2;
+//          }
+//        }
+//
+//        if (flower_inserted >= n) {
+//          return true;
+//        }
+//      }
+//      return false;
+//    }
+    bool canPlaceFlowers(vector<int> &flowerbed, int n) {
+      if (n == 0)
+        return true;
+      int flower_inserted = 0;
+
+      auto get = [&flowerbed](int idx){
+        if (idx <0 || idx >=flowerbed.size()) {
+          return 0;
+        }
+        return flowerbed[idx];
+      };
+
+      int idx = 0;
+      while(idx < flowerbed.size()) {
+        if (get(idx-1)==0 && get(idx)==0 && get(idx+1)==0) {
+          flowerbed[idx] = 1;
+          flower_inserted++;
+          idx+=2;
+        }else {
+          idx++;
+        }
+        if (flower_inserted >= n) {
+          return true;
+        }
+      }
+
+      return false;
+    }
+  };
+}
+
+TEST(mid, T452) { // 重做
+  class Solution {
+   public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+      auto compare = [](const vector<int>& lhs, const vector<int>& rhs) {
+        int l_back = lhs.back();
+        int r_back = rhs.back();
+        return l_back > r_back;
+      };
+      int n=0;
+      std::sort(points.begin(), points.end(), compare);
+      std::vector<bool> is_selected(points.size());
+
+      std::pair<int,int> range{points[0].front(), points[0].back()};
+      n=1;
+      for (size_t i=1; i<points.size(); i++) {
+        if(points[i].back() >= range.first) {
+          range = {std::max(range.first,points[i].front()),points[i].back()};
+        }else {
+          range = {points[i].front(), points[i].back()};
+          n++;
+        }
+      }
+
+      return n;
+    }
+  };
+
+  Solution slo;
+  std::vector<std::vector<int>> points = {{10,16},{2,8},{1,6},{7,12}};
+  EXPECT_EQ(2, slo.findMinArrowShots(points));
+  points = {{1, 2},{4, 5},{1, 5}};
+  EXPECT_EQ(2, slo.findMinArrowShots(points));
+  points = {{0,9},{1,8},{7,8},{1,6},{9,16},{7,13},{7,10},{6,11},{6,9},{9,13}};
+  EXPECT_EQ(3, slo.findMinArrowShots(points));
 }
 
 
+TEST(mid,T763) {
+  class Solution {
+   public:
+    vector<int> partitionLabels(const string& s) {
+      std::vector<int> slip;
+      std::unordered_map<char, std::pair<int, int>> char_num;// 纪录每个字母最小区间
+      for(int i=0; i< s.size(); i++) {
+        auto it = char_num.find(s[i]);
+        if (it == char_num.end()) {
+          char_num[s[i]] = {i,i};
+        } else {
+          it->second.second = i;
+        }
+      }
+
+      int index = 0;
+      int num = 0;
+      int range_end = 0;
+      while (index < s.size()) {
+        char c = s[index];
+        range_end = std::max(char_num[c].second,range_end);
+        num++;
+        if (range_end == index) {
+          slip.push_back(num);
+          num = 0;
+          range_end = index+1;
+        }
+        index++;
+      }
+      return slip;
+    }
+  };
+
+  Solution slo;
+  std::vector<int> slip;
+  slip = {1,1,1,1};
+  EXPECT_EQ(slo.partitionLabels("abcd"), slip);
+  slip = {9,7,8};
+  EXPECT_EQ(slo.partitionLabels("ababcbacadefegdehijhklij"), slip);
 }
+
+TEST(easy, T122) {
+  class Solution {
+   public:
+    int maxProfit(vector<int> &prices) {
+      int profit = 0;
+      int buy_price = -1;
+      for (int i = 1; i < prices.size(); i++) {
+        if (prices[i - 1] < prices[i]) { // price up
+          if (buy_price == -1) {
+            buy_price = prices[i - 1];
+          }
+        } else if (prices[i - 1] > prices[i]) { //price down
+          if (buy_price != -1) {
+            profit += prices[i - 1] - buy_price;
+            buy_price = -1;
+          }
+        }
+      }
+      if (buy_price != -1) {
+        profit += prices.back() - buy_price;
+      }
+      return profit;
+    }
+  };
+}
+
+TEST(mid, T406) {
+  class Solution {
+   public:
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+
+    }
+  };
+}
+
+} // namespace greedy algorithm
 
 
 int main(int argc, char **argv) {
